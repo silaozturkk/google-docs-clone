d358482a9d39d294fd096f358e832b7f2dca245e
Sıla Öztürk <sila.ozturk@stu.rumeli.edu.tr>
2025-07-14 11:21:59 +0300

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return ( 
+    <div className="flex min-h-screen items-center justify-center ">
+      <Button variant="destructive" size="lg">
+        Click me
+      </Button>
+    </div>
+   );
+}
+ 
+export default Home;

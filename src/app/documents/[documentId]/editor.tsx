4bbc6c6fdf56e1f3a4f8ccfdc9596c8a8a49da2b
Sıla Öztürk <sila.ozturk@stu.rumeli.edu.tr>
2025-07-16 14:33:21 +0300

    Font family & headings tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,21 +1,17 @@
 "use client";
 import TaskList from '@tiptap/extension-task-list'
 import TaskItem from '@tiptap/extension-task-item'
-
 import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-Cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
-
 import { Underline } from '@tiptap/extension-underline';
-
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style';
 import Image from '@tiptap/extension-image'
-
 import ImageResize from "tiptap-extension-resize-image"
-
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-
 import { useEditorStore } from '@/store/use-editor-store';
 
 
@@ -60,6 +56,8 @@ export const Editor = () => {
         extensions: [
             StarterKit, //metin yazabilir
             Underline,
+            FontFamily,
+            TextStyle,
             TaskList,
             Image, //resim ekleyebilir, boyutlandırabilir
             ImageResize,
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -4,8 +4,121 @@ import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
 import { Separator } from "@/components/ui/separator";
 
-import { BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquare, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon,  MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
 
+} from "@/components/ui/dropdown-menu";
+import { type Level } from "@tiptap/extension-heading";
+
+//yazı tipi boyutu için
+const HeadingLevelButton = () => {
+    const { editor } = useEditorStore();
+
+    const headings = [
+        { label: "normal text", value: 0, fontSize: "16px"},
+        { label: "Heading 1 ", value: 1, fontSize: "32px"},
+        { label: "Heading 2 ", value: 2, fontSize: "24px"},
+        { label: "Heading 3 ", value: 3, fontSize: "20px"},
+        { label: "Heading 4 ", value: 4, fontSize: "18px"},
+        { label: "Heading 5 ", value: 5, fontSize: "16px"},
+    ];
+
+    const getCurrentHeading = () => {
+        for (let level=1;level<=5;level++) {
+            if (editor?.isActive("heading", {level})) {
+                return `Heading ${level}`;
+            }
+        }
+
+        return "Normal text";
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild >
+                <button
+                    className={cn(
+                        "h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"  
+                    )}
+                >
+                    <span className="truncate">
+                        {getCurrentHeading()}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {headings.map(({ label, value, fontSize }) => (
+                    <button
+                        key={value}
+                        style={{ fontSize }}
+                        onClick={() => {
+                            if(value === 0) {
+                                editor?.chain().focus().setParagraph().run();
+                            }
+                            else {
+                                editor?.chain().focus().toggleHeading({ level: value as Level}).run();
+                            }
+                        }}
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 py-1 font-[value] rounded-sm hover:bg-neutral-200/80",
+                            (value===0 && !editor?.isActive("heading")) || editor?.isActive("heading",{level:value}) && "bg-neutral-200/80"
+                        )}
+                    >
+                        {label}
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+};
+ 
+// fontlar için ilk olarak dizi olusturduk daha sonra da açılır kapanır menu olsuturduk
+const FontFamilyButton = () => {
+    const { editor } = useEditorStore();
+    const fonts = [
+        { label: "Arial", value: "Arial"},
+        { label: "Times New Roman", value: "Times New Roman"},
+        { label:"Courier New", value: "Courier New"},
+        { label:"Georgia", value: "Georgia"},
+        { label:"Verdana", value: "Verdana"},
+    ];
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className={cn(
+                    "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"  
+                )}
+                >
+                    <span className="truncate">
+                        {editor?.getAttributes("textStyle").FontFamily || "Arial"}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {fonts.map(({label,value}) => (
+                    <button
+                        onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+                        key={value}
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 py-1 font-[value] rounded-sm hover:bg-neutral-200/80",
+                            editor?.getAttributes("textStyle").FontFamily === value && "bg-neutral-200/80"
+                        )}
+                        style={{ fontFamily: value }}
+                    >
+                        <span className="text-sm">{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 
 // özellik tanımlaması yaptık burda
 interface ToolbarButtonProps {
@@ -119,9 +232,9 @@ const Toolbar = () => {
             ))}
             {/* ayırıcı koyduk buraya components de var */}
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-            {/* TODO: Font family */}
+            <FontFamilyButton />
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-            {/* TODO: Heading */}
+            <HeadingLevelButton />
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
             {/* TODO: Font size */}
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />

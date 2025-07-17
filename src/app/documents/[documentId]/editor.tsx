f7e62cf2cdafc98ef5ef31d9e6b0e1c0269dac02
Sıla Öztürk <sila.ozturk@stu.rumeli.edu.tr>
2025-07-17 14:13:30 +0300

    Margin ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -19,12 +19,14 @@ import Color from "@tiptap/extension-color";
 import  Highlight  from '@tiptap/extension-highlight';
 import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
+import Ruler from './ruler';
 
 export const Editor = () => {
     const { setEditor } = useEditorStore();
     
     // editörü burda baslatırız içeriğini ve davranısını veririz.
     const editor = useEditor({
+        immediatelyRender: false,
         onCreate({ editor }) {
             setEditor(editor);
         },
@@ -109,6 +111,7 @@ export const Editor = () => {
 
     return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+            <Ruler />
             <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
                 <EditorContent editor={editor} />
             </div>

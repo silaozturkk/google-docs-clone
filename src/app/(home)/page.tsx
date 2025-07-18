dd0c4d51458957985320f640c75641efe94449c6
Sıla Öztürk <sila.ozturk@stu.rumeli.edu.tr>
2025-07-18 11:54:35 +0300

    Template gallery component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Navbar } from "./navbar";
+import TemplatesGallery from "./templates-gallery";
 
 const Home = () => {
   return ( 
@@ -8,9 +9,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        Click <Link href="/documents/123"> 
-        <span className="text-blue-500 text-underline">&nbsp;here&nbsp;</span>
-        </Link> to go to document id
+        <TemplatesGallery />
       </div>
     </div>
    );
